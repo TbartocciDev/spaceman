eb286641e43b761de0bbd8a17cfd8ae0474256a2
TbartocciDev <tommybartocci@gmail.com>
2023-03-16 17:11:08 -0400

    main element divs displayed in body

--- js/main.js
+++ js/main.js
@@ -14,15 +14,15 @@ const row3Div = document.querySelector('.row3')
 
 // adding letters for keyboards
 row1Letters.forEach((letter) => {
-    console.log(letter)
-    let letterEl = document.createElement('div')
-    letterEl.innerHTML = letter
-    letterEl.style.border = 'solid'
+    // console.log(letter)
+    // let letterEl = document.createElement('div')
+    // letterEl.innerHTML = letter
+    // letterEl.style.border = 'solid'
 
-    const elWdith = (row1Div.offsetWidth / row1Letters.length)
+    // const elWdith = (row1Div.offsetWidth / row1Letters.length)
 
-    letterEl.style.width = elWdith + "%"
-    console.log(elWdith + "%")
+    // letterEl.style.width = elWdith + "%"
+    // console.log(elWdith + "%")
 
-    row1Div.appendChild(letterEl)
+    // row1Div.appendChild(letterEl)
 })
