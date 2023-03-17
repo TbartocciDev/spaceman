e17e633bcd3fdc8be2b8a8cf3921412ac582bccc
TbartocciDev <tommybartocci@gmail.com>
2023-03-17 09:52:25 -0400

    Added keyboard elements

--- js/main.js
+++ js/main.js
@@ -12,17 +12,48 @@ const row1Div = document.querySelector('.row1')
 const row2Div = document.querySelector('.row2')
 const row3Div = document.querySelector('.row3')
 
+// height and width of keyboard div and letters
+const keyboardHeight = row1Div.offsetHeight
+const letterHeight = (keyboardHeight/10)
+const keyboardWdith = row1Div.offsetWidth
+const letterWidth = ((keyboardWdith / row1Letters.length) / 10) + 'vh'
+
+// single spot for letter styles
+const letterFontSize = '5.5vh'
+
 // adding letters for keyboards
+// top row
 row1Letters.forEach((letter) => {
-    // console.log(letter)
-    // let letterEl = document.createElement('div')
-    // letterEl.innerHTML = letter
-    // letterEl.style.border = 'solid'
+    let letterEl = document.createElement('div')
+    letterEl.style.border = 'solid'
+    letterEl.style.textAlign = 'center'
+    letterEl.innerHTML = letter.toUpperCase()
+    letterEl.style.width = letterWidth
+    letterEl.style.fontSize = letterFontSize
 
-    // const elWdith = (row1Div.offsetWidth / row1Letters.length)
+    row1Div.appendChild(letterEl)
+})
 
-    // letterEl.style.width = elWdith + "%"
-    // console.log(elWdith + "%")
+// second row
+row2Letters.forEach((letter) => {
+    let letterEl = document.createElement('div')
+    letterEl.style.border = 'solid'
+    letterEl.style.textAlign = 'center'
+    letterEl.innerHTML = letter.toUpperCase()
+    letterEl.style.width = letterWidth
+    letterEl.style.fontSize = letterFontSize
 
-    // row1Div.appendChild(letterEl)
+    row2Div.appendChild(letterEl)
+})
+
+// third row
+row3Letters.forEach((letter) => {
+    let letterEl = document.createElement('div')
+    letterEl.style.border = 'solid'
+    letterEl.style.textAlign = 'center'
+    letterEl.innerHTML = letter.toUpperCase()
+    letterEl.style.width = letterWidth 
+    letterEl.style.fontSize = letterFontSize
+
+    row3Div.appendChild(letterEl)
 })
